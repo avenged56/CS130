8cb35990984b6d8f7aab05764f9ffbd35c4c02f9
James Thompson <jsthompson@pipeline.sbcc.edu>
2012-04-25 15:31:39 -0700

    initial project version

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : JThompsonAddressBook.c
+ Author      : James Thompson
+ Version     :
+ Copyright   : asdf
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("asdf"); /* prints asdf */
+	return EXIT_SUCCESS;
+}
